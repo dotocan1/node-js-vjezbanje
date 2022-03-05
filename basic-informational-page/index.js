b10e69223b18409f094729e68e82d2ea1facfcbb
Dominik Otocan <dominik.otocan1@gmail.com>
2022-03-05 19:56:19 +0100

    basic page done

--- basic-informational-page/index.js
+++ basic-informational-page/index.js
@@ -6,12 +6,37 @@ const hostname = "localhost"
 
 const server = http.createServer((req, res) => {
     const myURL = req.url
-    if(myURL == '/404.html'){
-        fs.readFile("./", () => {
-            console.log("No such file!")
+    if (myURL == '/') {
+        fs.readFile('./index.html', function (err, data) {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end;
+        })
+    } else if (myURL == '/contact-me.html') {
+        fs.readFile('./contact-me.html', function (err, data) {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end;
+        })
+    } else if (myURL == '/about.html') {
+        fs.readFile('./about.html', function (err, data) {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end;
+        })
+    } else{
+        fs.readFile('./404.html', function (err, data) {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end;
         })
     }
-})
+}
+)
 
 server.listen(port, () => {
     console.log(`Server running at port http://${hostname}:${port}`)
